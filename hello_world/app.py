25554b469ee6e525a79ac32420f98e7b4b5356c5
MaricchiaZ <mcaz90@gmail.com>
2023-05-17 16:46:44 -0300

    colocando o DynamoDB

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,4 +1,9 @@
-# import json
+import json
+import os
+import datetime
+import uuid
+
+import boto3
 
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 
@@ -19,6 +24,15 @@ def simple_post():
 #decorator, 
 @app.get("/hello")
 def hello():
+    params = {
+        "id": str(uuid.uuid4()),
+        "created_dt": str(datetime.datetime.now()),
+        "summary": "Action Name",
+        "description": "Action Description",
+        "priority": "High",
+    }
+    db_response = dynamo_table().put_item(Item=params)
+    # print(db_response)
     return {"read": "hello get!"}
 
 @app.delete("/hello")
@@ -32,6 +46,16 @@ def hello_patch():
 def lambda_handler(event, context):
     return app.resolve(event, context)
 
+def dynamo_table():
+    table_name = os.environ.get("TABLE", "Actions")
+    region = os.environ.get("REGION", "us-east-1")
+    aws_environment = os.environ.get("AWSENV", "AWS")
+    if aws_environment == "AWS_SAM_LOCAL":
+        actions_table = boto3.resource("dynamodb", endpoint_url="http://dynamodb:8000")
+    else:
+        actions_table = boto3.resource("dynamodb", endpoint_url="http://localhost:4566", region_name=region)
+    return actions_table.Table(table_name)
+
 # curl -X 'POST' http://127.0.0.1:3000/hello/ \
 # -H 'Content-Type: application/json' \
 # -d '
@@ -40,3 +64,9 @@ def lambda_handler(event, context):
 # "name": "string",
 # "description": "string"
 # }'
+
+
+# samlocal build && samlocal local start-api
+
+#  curl -X 'PATCH' http://127.0.0.1:3000/hello
+# http://localhost:4566/restapis/ja58lkf4pb/local/_user_request_/hello
